507493ec793687f860b73d76f261eff36ecf1c74
adivis <adi.vishwakarma01@gmail.com>
2024-02-24 22:49:19 +0530

    added changes to host publically

--- application.py
+++ application.py
@@ -123,4 +123,4 @@ def paraphrase():
             return render_template("display_text.html", data="Error has occured")
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(debug=False, host='0.0.0.0')
